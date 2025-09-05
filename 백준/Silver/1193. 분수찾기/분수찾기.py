31b522eddd129437b608f50293a630144364e5a1
KSW8515 <lskngl0125@gmail.com>
2025-09-06 03:53:53 +0900

    [Silver V] Title: 분수찾기, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,15 +1,13 @@
+import math
+
 n = int(input())
 
-num_sum = 0
-line = 1
+line = math.ceil((-1 + math.sqrt(1 + 8 * n)) / 2)
+
+num_sum = line * (line + 1) / 2
+num_sum = int(num_sum)
 
 move_count = 0
-
-while True:
-    num_sum += line
-    if num_sum >= n:
-        break
-    line += 1
 
 if n != 1 and n - (num_sum - line) > 0:
     move_count = n - (num_sum - line) - 1
