1192655a6ea73f5017932e9c981c781cdfed4faa
KSW8515 <lskngl0125@gmail.com>
2025-09-06 00:49:30 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+count = n
+
+for _ in range(n):
+    s = input()
+    for i in range(len(s)-1):
+        if i == (len(s) - 1):
+            break
+        if s[i] == s[i + 1]:
+            pass
+        elif s[i] in s[i + 1:]:
+            count -= 1
+            break
+print(count)
