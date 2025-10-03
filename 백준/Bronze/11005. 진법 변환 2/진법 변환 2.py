66a15980c0dcab8819b5b32f9ed69b088205c889
KSW8515 <lskngl0125@gmail.com>
2025-10-03 14:04:07 +0900

    [Bronze I] Title: 진법 변환 2, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.py
@@ -1,8 +1,17 @@
 N, B = map(int, input().split(' '))
 
-arr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+temp = 0
 result = ""
-while N:
-    result += arr[N%B]
+while True:
+    y = N % B
     N = N//B
+
+    if y > 9:
+        y += 55
+        y = chr(y)
+    result += str(y)
+
+    if N == 0:
+        break
+
 print(result[::-1])
